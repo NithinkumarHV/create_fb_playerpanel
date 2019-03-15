0a419f73dd0fdf5375d87e03db83b52d0508cd6a
Nithin_Kumar_HV <nithinkumarhv01@gmail.com>
2019-03-15 19:52:58 +0530

    error redux dev tools resolved

--- src/store.js
+++ src/store.js
@@ -60,7 +60,7 @@ const store = createStoreWithFirebase(
   initialState,
   compose(
     reactReduxFirebase(firebase),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
   )
 );
 
